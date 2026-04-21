ba38d2ef0d8bc096b9e6cc7dcc73921465850dc1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-04-21 08:53:17 -0700

    refactor(headers): unify raw header normalization

--- test/node-test/util.js
+++ test/node-test/util.js
@@ -159,6 +159,8 @@ test('parseHeaders decodes array header values as latin1', () => {
 })
 
 test('parseRawHeaders', () => {
+  assert.deepEqual(util.parseRawHeaders(), [])
+  assert.deepEqual(util.parseRawHeaders(null), [])
   assert.deepEqual(util.parseRawHeaders(['key', 'value', Buffer.from('key'), Buffer.from('value')]), ['key', 'value', 'key', 'value'])
   assert.deepEqual(util.parseRawHeaders(['content-length', 'value', 'content-disposition', 'form-data; name="fieldName"']), ['content-length', 'value', 'content-disposition', 'form-data; name="fieldName"'])
   assert.deepEqual(util.parseRawHeaders({ key: 'value', 'set-cookie': ['a=1', 'b=2'] }), ['key', 'value', 'set-cookie', 'a=1', 'set-cookie', 'b=2'])
