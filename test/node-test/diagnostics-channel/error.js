3bf98db7df09d3c8d3f8e323a1da26d38892059d
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-05-06 11:32:42 -0700

    test: make diagnostics-channel error test isolate its request

--- test/node-test/diagnostics-channel/error.js
+++ test/node-test/diagnostics-channel/error.js
@@ -6,6 +6,10 @@ const diagnosticsChannel = require('node:diagnostics_channel')
 const { Client } = require('../../..')
 const { createServer } = require('node:http')
 
+function isSocketErrorCode (code) {
+  return code === 'UND_ERR_SOCKET' || code === 'ECONNRESET'
+}
+
 test('Diagnostics channel - error', (t) => {
   const assert = tspl(t, { plan: 3 })
   const server = createServer({ joinDuplicateHeaders: true }, (req, res) => {
@@ -13,24 +17,42 @@ test('Diagnostics channel - error', (t) => {
   })
   after(server.close.bind(server))
 
+  const createChannel = diagnosticsChannel.channel('undici:request:create')
+  const errorChannel = diagnosticsChannel.channel('undici:request:error')
+
   const reqHeaders = {
     foo: undefined,
     bar: 'bar'
   }
 
   let _req
-  diagnosticsChannel.channel('undici:request:create').subscribe(({ request }) => {
-    _req = request
-  })
-
-  diagnosticsChannel.channel('undici:request:error').subscribe(({ request, error }) => {
-    assert.equal(_req, request)
-    assert.equal(error.code, 'UND_ERR_SOCKET')
-  })
 
   return new Promise((resolve) => {
     server.listen(0, () => {
-      const client = new Client(`http://localhost:${server.address().port}`, {
+      const origin = `http://localhost:${server.address().port}`
+
+      const createHandler = ({ request }) => {
+        if (request.origin === origin && request.path === '/' && request.method === 'GET') {
+          _req = request
+        }
+      }
+      createChannel.subscribe(createHandler)
+
+      const errorHandler = ({ request, error }) => {
+        if (request !== _req) {
+          return
+        }
+
+        assert.equal(_req, request)
+        assert.ok(isSocketErrorCode(error.code), error.code)
+      }
+      errorChannel.subscribe(errorHandler)
+      t.after(() => {
+        createChannel.unsubscribe(createHandler)
+        errorChannel.unsubscribe(errorHandler)
+      })
+
+      const client = new Client(origin, {
         keepAliveTimeout: 300e3
       })
 
@@ -39,7 +61,7 @@ test('Diagnostics channel - error', (t) => {
         method: 'GET',
         headers: reqHeaders
       }, (err, data) => {
-        assert.equal(err.code, 'UND_ERR_SOCKET')
+        assert.ok(isSocketErrorCode(err.code), err.code)
         client.close()
         resolve()
       })
